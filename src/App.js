02daedeef2866fafb8b16491b9a708c770f0af8c
unknown <yilmaz.deniz.itu@gmail.com>
2021-04-13 17:46:05 +0200

    initial app
    initial app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [seasons, setSeasons] = useState([])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Rick and morty</h2>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
